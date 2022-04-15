a091960ce03d4e8da78d2ab047042558b47244be
ilyas <73260385+Mr-TalhaIlyas@users.noreply.github.com>
2022-04-15 17:30:25 +0900

    Update fmutils.py

--- scripts/fmutils.py
+++ scripts/fmutils.py
@@ -142,6 +142,121 @@ def get_random_files(main_dir, count=1):
     
     return file_path
 
+def rename_wrt_dirname(main_dir):
+    '''
+    Change the names of all files inside the main_dir wrt their sub_dir names.
+    e.g:
+    main_dir
+    │
+    ├── dir1\
+    │   ├── ADE_train_00000983.jpg
+    │   ├── ADE_train_00000984.jpg
+    │   ├── ADE_train_00000994.png
+    │
+    ├── dir2\
+        ├── ADE_train_00000983.jpg
+        └── redme.txt
+    
+    will change to ->
+    main_dir
+    │
+    ├── dir1\
+    │   ├── dir1_001.jpg
+    │   ├── dir1_002.jpg
+    │   ├── dir1_003.png
+    │
+    ├── dir2\
+        ├── dir2_004.jpg
+        └── dir2_005.txt
+    
+    Parameters
+    ----------
+    main_dir :  main directory containing all sub dirs.
+    -------
+    None.
+
+    '''
+    main_dir = main_dir +'/'
+    x = get_all_files(main_dir)
+
+    for i in trange(len(x), desc="Changing names of files"):
+        path = os.path.dirname(x[i])
+        ext_name = os.path.basename(x[i]).split('.')[-1]
+        new_name = os.path.dirname(x[i]).split('/')[-1] + f'_{i:03d}.' + ext_name
+        os.rename(x[i],
+                  os.path.join(os.path.dirname(x[i]), new_name))
+    return None
+
+def file_name_replacer(data_dir, new_name, name2replace):
+    '''
+    Changes the names of all files inside a dir by replacing the specific strings
+    in old file name with new ones, specified via 2 input lists.
+    e.g.
+    if:
+    new_name = [train, iris]
+    name2replace = [new_2, new_3] then, this
+    
+    ..\test_dir\
+    │
+    ├── dir1\
+    │   ├── ADE_train_00000983.jpg
+    │   ├── ADE_train_00000984.jpg
+    │   ├── ADE_train_00000994.png
+    ├── dir2\
+    │   ├── sub_dir1\
+    │   │   └── sub_sub_dir1\
+    │   │       ├── housing.csv
+    │   │       ├── iris.csv
+    │   │       ├── mnist_test_300.csv
+    │   │       └── mnist_train_3000.csv
+    
+    will change to ->
+    
+    \test_dir\
+    │
+    ├── dir1\
+    │   ├── ADE_new_2_00000983.jpg
+    │   ├── ADE_new_2_00000984.jpg
+    │   ├── ADE_new_2_00000994.png
+    ├── dir2\
+    ├── sub_dir1\
+    │   └── sub_sub_dir1\
+    │       ├── housing.csv
+    │       ├── mnist_new_2_3000.csv
+    │       ├── mnist_test_300.csv
+    │       └── new_3.csv
+    
+    Note : both lists should have same length
+    Parameters
+    ----------
+    data_dir : main dir containig all the sub dir.
+    new_name : list containing the new names which will replace the old ones.
+    name2replace : list containing the strings which will be replaced wiht new ones.
+
+    Returns
+    -------
+    None.
+
+    '''
+    full_paths = get_all_files(data_dir + '/')
+    
+    k_name = name2replace
+    e_name = new_name
+    
+    new_full_paths = []
+    
+    for i in full_paths:
+        name = os.path.basename(i)
+        for j in range(len(k_name)):        
+            temp = name.replace(k_name[j], e_name[j])
+            name = temp
+        #new_names.append(name)
+        
+        new_full_paths.append(os.path.join(os.path.dirname(i), name)) 
+    [os.rename(full_paths[c], new_full_paths[c]) for c in range(len(full_paths))]
+    
+    return None
+
 
 def del_all_files(main_dir, confirmation=True):
     '''
