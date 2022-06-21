42991e59a98df2216d05f79950ea515e4398f92c
ilyas <73260385+Mr-TalhaIlyas@users.noreply.github.com>
2022-06-21 17:07:11 +0900

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Talha Ilyas'
 author = 'Talha Ilyas'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
