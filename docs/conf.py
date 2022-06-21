dcb4a836dff8b2ef6ec26521ca28aa169d6b0299
ilyas <73260385+Mr-TalhaIlyas@users.noreply.github.com>
2022-06-21 18:01:36 +0900

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-import src.fmutils.fmutils
-import src.fmutils.directorytree
+import src.fmutils
 # -- Project information -----------------------------------------------------
 
 project = 'File Management Utils'
