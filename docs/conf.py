ff2ae2bda2c974e3814664cc4fe0d1c9413b7206
ilyas <73260385+Mr-TalhaIlyas@users.noreply.github.com>
2023-10-27 10:38:42 +0900

    0.2.2 conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ copyright = '2022, Talha Ilyas'
 author = 'Talha Ilyas'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.1'
+release = '0.2.2'
 
 
 # -- General configuration ---------------------------------------------------
